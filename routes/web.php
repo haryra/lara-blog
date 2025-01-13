84042297be4577253e26be570cc3499792e43801
haryra <haryraachmat10@gmail.com>
2025-01-13 15:23:26 +0700

    View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -14,8 +15,48 @@ Route::get('/about', function () {
     return view('about', ['nama' => 'Hary R','title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Hary R',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quo, voluptate quibusdam vel obcaecati impedit labore minima pariatur maiores optio magni hic ullam inventore dicta animi expedita magnam. Dicta, perspiciatis!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Hary R',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quo quod molestiae ipsa non veniam odio totam, ad saepe perferendis tenetur ex excepturi? In delectus corporis id veritatis ratione illo?'
+        ],
+    ]]);
+});
+
+Route::get('/posts/{slug}', function($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Hary R',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quo, voluptate quibusdam vel obcaecati impedit labore minima pariatur maiores optio magni hic ullam inventore dicta animi expedita magnam. Dicta, perspiciatis!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Hary R',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quo quod molestiae ipsa non veniam odio totam, ad saepe perferendis tenetur ex excepturi? In delectus corporis id veritatis ratione illo?'
+        ]
+        ];
+
+        $post = Arr::first($posts, function($post) use ($slug) {
+            return $post['slug'] == $slug;
+        });
+
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
